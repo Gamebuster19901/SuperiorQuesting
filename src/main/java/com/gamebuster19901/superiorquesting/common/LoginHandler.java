a9debc6f37c5e4510f10d86f013575876f6f51ce
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-08-22 23:33:48 -0400

    Made a login handler which gives new players 1 quest book when they first join, if the config allows.

---
+++ src/main/java/com/gamebuster19901/superiorquesting/common/LoginHandler.java
@@ -0,0 +1,30 @@
+package com.gamebuster19901.superiorquesting.common;
+
+import static com.gamebuster19901.superiorquesting.Main.MODID;
+
+import com.gamebuster19901.superiorquesting.common.item.ItemQuestBook;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+
+public class LoginHandler extends MultiplayerHandler{
+	private static final String LOGIN_KEY = MODID + ":loggedin"; 
+	@Override
+	protected void onConfigFinishChanged() {}
+
+	@Override
+	protected void playerLoggedIn(PlayerLoggedInEvent e) {
+		if(!getPersistantTag((EntityPlayerMP)e.player).hasKey(LOGIN_KEY)){
+			giveBook(e.player);
+			e.player.getEntityData().setBoolean(LOGIN_KEY, true);
+		}
+	}
+	
+	private void giveBook(EntityPlayer p){
+		if(ModConfig.RULES.hasBook){
+			p.inventory.addItemStackToInventory(new ItemStack(ItemQuestBook.ITEM, 1));
+		}
+	}
+}
--- src/main/java/com/gamebuster19901/superiorquesting/proxy/Proxy.java
+++ src/main/java/com/gamebuster19901/superiorquesting/proxy/Proxy.java
@@ -4,6 +4,8 @@ import com.gamebuster19901.superiorquesting.Main;
 import com.gamebuster19901.superiorquesting.client.gui.GuiHandler;
 import com.gamebuster19901.superiorquesting.common.Debuggable;
 import com.gamebuster19901.superiorquesting.common.LifeHandler;
+import com.gamebuster19901.superiorquesting.common.LoginHandler;
+import com.gamebuster19901.superiorquesting.common.MultiplayerHandler;
 import com.gamebuster19901.superiorquesting.common.command.CommandLives;
 import com.gamebuster19901.superiorquesting.common.item.ItemHeartCanister;
 import com.gamebuster19901.superiorquesting.common.item.ItemQuestBook;
@@ -16,13 +18,16 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
 public abstract class Proxy implements Debuggable{
 	private static final LifeHandler LIFE_HANDLER = new LifeHandler();
+	private static final LoginHandler LOGIN_HANDLER = new LoginHandler();
 	
 	public void preInit(FMLPreInitializationEvent e){
 		MinecraftForge.EVENT_BUS.register(LIFE_HANDLER);
+		MinecraftForge.EVENT_BUS.register(LOGIN_HANDLER);
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 	
