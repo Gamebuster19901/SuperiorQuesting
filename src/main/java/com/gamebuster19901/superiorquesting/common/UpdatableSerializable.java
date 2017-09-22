bf50b2d12be207b3b4376d3bbc654b069a83c654
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-09-22 03:09:46 -0400

    remove default implementation, would rather have compiler error than to do unexpected behavior silently

--- src/main/java/com/gamebuster19901/superiorquesting/common/UpdatableSerializable.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/UpdatableSerializable.java
@@ -3,16 +3,13 @@ package com.gamebuster19901.superiorquesting.common;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 public interface UpdatableSerializable extends Serializable{
 	public void convert(int prevVersion, int nextVersion, ObjectInputStream in);
 	
-	public default void writeObject(ObjectOutputStream out) throws IOException{
-		out.defaultWriteObject();
-	}
-	public default void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException{
-		in.defaultReadObject();
-	}
+	public void writeObject(ObjectOutputStream out) throws IOException;
+	
+	public void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException;
+	
 }
