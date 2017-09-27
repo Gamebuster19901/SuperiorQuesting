5b24fbb3853aefd2e44a3b227919a3d4caad583a
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-09-26 23:02:48 -0400

    Begin creation of quest command.

--- src/main/java/com/gamebuster19901/superiorquesting/common/command/CommandQuest.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/command/CommandQuest.java
@@ -1,20 +1,30 @@
 package com.gamebuster19901.superiorquesting.common.command;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import static com.gamebuster19901.superiorquesting.Main.MODID;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.gamebuster19901.superiorquesting.Main;
 import com.gamebuster19901.superiorquesting.common.Debuggable;
+import com.gamebuster19901.superiorquesting.common.item.ItemQuestBook;
+import com.gamebuster19901.superiorquesting.common.questing.QuestHandler;
 
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.PlayerNotFoundException;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.rcon.RConConsoleSource;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.CommandBlockBaseLogic;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class CommandQuest extends CommandBase implements ICommand, Debuggable{
@@ -26,20 +36,17 @@ public class CommandQuest extends CommandBase implements ICommand, Debuggable{
 	
 	@Override
 	public int compareTo(ICommand o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return "quest";
 	}
 
 	@Override
 	public String getUsage(ICommandSender sender) {
-		// TODO Auto-generated method stub
-		return "";
+		return "/quest edit";
 	}
 	
 	@Override
@@ -51,8 +58,46 @@ public class CommandQuest extends CommandBase implements ICommand, Debuggable{
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		// TODO Auto-generated method stub
+		QuestHandler questhandler = Main.proxy.getQuestHandler();
+		if (args.length == 0){
+			if(sender instanceof EntityPlayerMP){
+				EntityPlayerMP p = (EntityPlayerMP)sender;
+				int questsCompleted = questhandler.getCompletedQuests(p).size();
+				int totalQuests = questhandler.getAllQuests().size();
+				p.sendMessage(new TextComponentString("Quests completed: " + questsCompleted + "/" + totalQuests ));
+				return;
+			}
+		}
+		if(!sender.canUseCommand(2, "give")){
+			throw new CommandException("commands.generic.permission");
+		}
 		
+		if(args.length == 1) {
+			if(args[0].equals("edit")) {
+				if(sender instanceof EntityPlayerMP) {
+					EntityPlayerMP p = (EntityPlayerMP)sender;
+					p.addItemStackToInventory(new ItemStack(ItemQuestBook.ITEM, 1, 1));
+					return;
+				}
+			}
+		}
+		
+		if(args.length == 2) {
+			if(args[0].equals("edit")) {
+				if(questhandler.getPlayerFromUsername(args[1]) != null) {
+					
+				}
+				else if(questhandler.getPlayerEntityFromUUID(UUID.fromString(args[1])) != null) {
+					
+				}
+				else if(questhandler.getNbtOfPlayerUsingUUID(UUID.fromString(args[1])) != null) {
+					
+				}
+				throw new PlayerNotFoundException("commands.generic.player.notFound", new Object[] {args[1]});
+			}
+		}
+		
+		throw new WrongUsageException(MODID + ".commands.lives.usage");
 	}
 
 	@Override
