601504ad021c83f826d0109a3372031ef78cbff4
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-10-11 22:08:39 -0400

    Allow static assertions

--- src/main/java/com/gamebuster19901/superiorquesting/common/Assertable.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/Assertable.java
@@ -59,4 +59,59 @@ public interface Assertable {
 		}
 		return true;
 	}
+	
+	/**
+	 * throws an AssertionError if the assertion is false
+	 * @param assertion
+	 * @throws AssertionError
+	 * @return true
+	 */
+	public static boolean Assert(boolean assertion, Object unused) throws AssertionError{
+		if(!assertion){
+			throw new AssertionError();
+		}
+		return true;
+	}
+	
+	/**
+	 * throws an AssertionError with the specified message if the assertion is false
+	 * @param assertion
+	 * @throws AssertionError
+	 * @return true
+	 */
+	public static boolean Assert(boolean assertion, String failedMessage, Object unused) throws AssertionError{
+		if(!assertion){
+			throw new AssertionError(failedMessage);
+		}
+		return true;
+	}
+	
+	/**
+	 * throws an AssertionError with the specified cause if the assertion is false
+	 * @param assertion
+	 * @param cause, may be null
+	 * @throws AssertionError
+	 * @return true
+	 */
+	public static boolean Assert(boolean assertion, Throwable cause, Object unused) throws AssertionError{
+		if(!assertion){
+			throw new AssertionError(cause);
+		}
+		return true;
+	}
+	
+	/**
+	 * throws an AssertionError with the specified message and cause if the assertion is false
+	 * @param assertion
+	 * @param failedMessage
+	 * @param cause, may be null
+	 * @throws AssertionError
+	 * @return true
+	 */
+	public static boolean Assert(boolean assertion, String failedMessage, Throwable cause, Object unused) throws AssertionError{
+		if(!assertion){
+			throw new AssertionError(failedMessage, cause);
+		}
+		return true;
+	}
 }
