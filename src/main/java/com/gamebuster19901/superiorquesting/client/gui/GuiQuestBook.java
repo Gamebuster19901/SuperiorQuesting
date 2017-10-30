cbbd6ea9c2bb716250490a072cef881d8c6001af
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-10-30 17:29:18 -0400

    Closing the Quest Book Gui now plays closing book sounds

--- src/main/java/com/gamebuster19901/superiorquesting/client/gui/GuiQuestBook.java
+++ src/main/java/com/gamebuster19901/superiorquesting/client/gui/GuiQuestBook.java
@@ -1,12 +1,25 @@
 package com.gamebuster19901.superiorquesting.client.gui;
 
+import com.gamebuster19901.superiorquesting.Main;
+import com.gamebuster19901.superiorquesting.proxy.ClientProxy;
+
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
 
 public final class GuiQuestBook extends GuiScreen{
 	private EntityPlayer player;
 	public GuiQuestBook(EntityPlayer player) {
-		// TODO Auto-generated constructor stub
+		this.player = player;
+	}
+	
+	@Override
+	public boolean doesGuiPauseGame() {
+		return false;
 	}
 
+	public void onGuiClosed(){
+		player.playSound(((ClientProxy)Main.proxy).BOOK_CLOSE, 1, 1);
+		super.onGuiClosed();
+	}
+	
 }
