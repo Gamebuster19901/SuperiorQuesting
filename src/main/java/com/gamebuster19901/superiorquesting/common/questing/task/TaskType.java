7e8d313c7fcb09c64a057f1d3063f2f4a9487adb
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-11-15 22:01:54 -0500

    Better validation to ensure that addons don't do stupid things.

--- src/main/java/com/gamebuster19901/superiorquesting/common/questing/task/TaskType.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/questing/task/TaskType.java
@@ -1,10 +1,14 @@
 package com.gamebuster19901.superiorquesting.common.questing.task;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 
 import com.gamebuster19901.superiorquesting.Main;
 import com.gamebuster19901.superiorquesting.common.questing.exception.DuplicateKeyException;
+import com.gamebuster19901.superiorquesting.common.questing.exception.MalformedTypeError;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 
 public final class TaskType {
@@ -28,16 +32,20 @@ public final class TaskType {
 	 * @param image the image to use when selecting a task type in the quest editor
 	 * @param name the name of the task
 	 * @param description the description of the task
+	 * @throws DuplicateKeyException if the task is already registered
+	 * @throws IllegalArgumentException if task does not extend Task.class
+	 * @throws MalformedTypeError if the task to register is not implemented correctly
 	 */
 	public static final void registerTaskType(Class<? extends Task> task, ResourceLocation image, String name, String description) {
 		for(TaskType r : taskTypes) {
 			if(r.task.equals(task)) {
 				throw new DuplicateKeyException("Cannot register a task type more than once: " + task.getName());
 			}
-			if(r.task.equals(Task.class)) {
-				throw new IllegalArgumentException("YOU'RE AN IDIOT, DON'T DO THAT");
-			}
 		}
+		if(task == Task.class || !(task.isAssignableFrom(Task.class))) {
+			throw new IllegalArgumentException(Task.class + " does not extend " + Task.class);
+		}
+		validateTask(task);
 		taskTypes.add(new TaskType(task, image, name, description));
 		Main.LOGGER.info("Registered new task type: " + name);
 	}
@@ -54,4 +62,18 @@ public final class TaskType {
 		}
 		return null;
 	}
+	
+	private static final void validateTask(Class<? extends Task> task) {
+		try {
+			Constructor<?>[] constructors = task.getConstructors();
+			Constructor nbtConstructor = task.getConstructor(MinecraftServer.class, NBTTagCompound.class);
+			if(constructors.length != 3) {
+				throw new MalformedTypeError(task.getName() + " must have 3 constructors");
+			}
+		}
+		catch(NoSuchMethodException e) {
+			throw new MalformedTypeError("Missing NBT Constructor", e);
+		}
+	}	
 }
+
