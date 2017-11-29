36e0e8e55aa1bc15e046b8643a084b2a681a27ca
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-11-29 11:38:29 -0500

    Fixed interacting with blocks/using off hand when opening quest book.

--- src/main/java/com/gamebuster19901/superiorquesting/common/item/ItemQuestBook.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/item/ItemQuestBook.java
@@ -9,7 +9,6 @@ import com.gamebuster19901.superiorquesting.client.gui.GuiHandler;
 import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.play.server.SPacketCustomSound;
@@ -40,19 +39,20 @@ public final class ItemQuestBook extends Item{
     {
 		if(p.getHeldItem(handIn).getItemDamage() == 0) {
 			p.openGui(Main.getInstance(), GuiHandler.QUEST_BOOK, worldIn, (int)p.posX, (int)p.posY, (int)p.posZ);
-			return new ActionResult<ItemStack>(EnumActionResult.PASS, p.getHeldItem(handIn));
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, p.getHeldItem(handIn));
 		}
 		else if(p.canUseCommand(2, "give")){
 			p.openGui(Main.getInstance(), GuiHandler.QUEST_BOOK, worldIn, (int)p.posX, (int)p.posY, (int)p.posZ);
-			return new ActionResult<ItemStack>(EnumActionResult.PASS, p.getHeldItem(handIn));
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, p.getHeldItem(handIn));
 		}
 		else if(!worldIn.isRemote){
 			((EntityPlayerMP) p).connection.sendPacket(new SPacketCustomSound("questing:bookvanish", SoundCategory.PLAYERS, p.posX, p.posY, p.posZ, 1, 1));
 			p.getHeldItem(handIn).setCount(0);
 			worldIn.addWeatherEffect(new EntityLightningBolt(worldIn, p.posX, p.posY, p.posZ, false));
 			worldIn.addWeatherEffect(new EntityLightningBolt(worldIn, p.posX, p.posY, p.posZ, false));
+			return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, ItemStack.EMPTY);
 		}
-		return new ActionResult<ItemStack>(EnumActionResult.PASS, ItemStack.EMPTY);
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, ItemStack.EMPTY);
     }
 	
     @SideOnly(Side.CLIENT)
