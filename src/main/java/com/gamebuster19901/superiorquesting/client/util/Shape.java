f15829ae653a2f1cf2b6fdbeb74641f56a569855
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-12-20 13:10:39 -0500

    Make debug shapes porportional

--- src/main/java/com/gamebuster19901/superiorquesting/client/util/Shape.java
+++ src/main/java/com/gamebuster19901/superiorquesting/client/util/Shape.java
@@ -81,7 +81,7 @@ public interface Shape {
 		
         for(int i = 0; i < arr.length; i++){
             for(int j = 0; j < arr[0].length; j++){
-                System.out.print(arr[i][j] ? "T" : "F");
+                System.out.print(arr[i][j] ? "TT" : "FF");
             }
             System.out.print('\n');
         }
