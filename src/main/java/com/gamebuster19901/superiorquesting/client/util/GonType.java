f91ce7fef6ab3f7b973b274765690cd8ce820db2
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-12-22 11:53:12 -0500

    Enum to get the amount of sides in a polygon

---
+++ src/main/java/com/gamebuster19901/superiorquesting/client/util/GonType.java
@@ -0,0 +1,29 @@
+package com.gamebuster19901.superiorquesting.client.util;
+
+public enum GonType {
+	PENTA(5),
+	HEXA(6),
+	SEPTA(7),
+	OCTA(8),
+	NONA(9),
+	DECA(10),
+	UNDECA(11),
+	DODECA(12);
+	
+	private int intVal;
+	
+	private GonType(int i) {
+		intVal = i;
+	}
+	
+	public String getGonType(int sides) {
+		if(sides > 4 && sides < 13) {
+			return (GonType.values()[sides - 5] + "gon").toLowerCase();
+		}
+		return sides + "-gon";
+	}
+	
+	public String toString() {
+		return this.toString().toLowerCase();
+	}
+}
