2e83c92dd0ad6c1a1d5eeb62a396363ffedcdaf5
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-12-22 14:48:06 -0500

    Rev to f74345b

--- src/main/java/com/gamebuster19901/superiorquesting/common/shape/ShapeType.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/shape/ShapeType.java
@@ -41,7 +41,6 @@ public class ShapeType {
 				throw new IllegalArgumentException("YOU'RE AN IDIOT, DON'T DO THAT");
 			}
 		}
-		System.out.println(specialParams.length);
 		for(Class c : specialParams) {
 			if(c.isPrimitive()) {
 				continue;
