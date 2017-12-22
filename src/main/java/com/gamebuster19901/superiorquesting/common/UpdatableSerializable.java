52cc07ac015fdb32bbef205dc0879ed4650b9714
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2017-12-22 11:54:12 -0500

    Duplicated deserializeNBT method so the parameter name is "data" by default instead of "nbt"

--- src/main/java/com/gamebuster19901/superiorquesting/common/UpdatableSerializable.java
+++ src/main/java/com/gamebuster19901/superiorquesting/common/UpdatableSerializable.java
@@ -1,9 +1,11 @@
 package com.gamebuster19901.superiorquesting.common;
 
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.INBTSerializable;
 
-public interface UpdatableSerializable extends INBTSerializable<NBTTagCompound>{
-	public void convert(long prevVersion, long nextVersion, NBTTagCompound in);
+public interface UpdatableSerializable extends INBTSerializable<NBTTagCompound>, Assertable, NBTDebugger, Debuggable{
+	public void convert(long prevVersion, long nextVersion, NBTTagCompound nbtIn);
 	public long getVersion();
+	public void deserializeNBT(NBTTagCompound data); //so it's named data by default
 }
